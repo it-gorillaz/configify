af1063e5e5ae119e57a837a95f3c3e9bcdb67914
tom-melo <neilton.demelo@emnify.com>
2024-01-15 22:38:51 +0000

    add value decorator

---
+++ src/decorators/value.decorator.ts
@@ -0,0 +1,47 @@
+import { ConfigurationRegistry } from '../configuration/configuration.registry';
+
+/**
+ * The value metadata.
+ */
+export const VALUE_METADATA = Symbol.for('__value__');
+
+/**
+ * The value properties metadata.
+ */
+export const VALUE_PROPERTIES_METADATA = Symbol.for('__properties__');
+
+/**
+ * The value options.
+ * Allows custom parsing to configuration values
+ */
+export interface ValueOptions {
+  parse: (value: any) => any;
+}
+
+/**
+ * The value decorated key interface.
+ */
+export interface ValueDecoratedKey {
+  key: string;
+  options?: ValueOptions;
+}
+
+/**
+ * The value decorator.
+ *
+ * This decorator defines which configuration key should
+ * be assined to the class attribute.
+ *
+ * @param   {string}            key     the configuration key
+ * @param   {ValueOptions}      options custom options
+ * @returns {PropertyDecorator}         the property decorator
+ */
+export const Value = (
+  key: string,
+  options?: ValueOptions,
+): PropertyDecorator => {
+  return (target: object, property: string) => {
+    ConfigurationRegistry.registerAttribute(target, property);
+    Reflect.defineMetadata(VALUE_METADATA, { key, options }, target, property);
+  };
+};
