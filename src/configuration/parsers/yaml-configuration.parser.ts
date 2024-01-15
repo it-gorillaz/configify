6a3883a1ebbc99ac0fc855040eeae0d637117da1
tom-melo <neilton.demelo@emnify.com>
2024-01-15 22:41:51 +0000

    add yaml configuration parser

---
+++ src/configuration/parsers/yaml-configuration.parser.ts
@@ -0,0 +1,18 @@
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
+import { ConfigurationParser } from '../configuration-parser.interface';
+
+/**
+ * YAML configuration parser
+ */
+export class YamlConfigurationParser implements ConfigurationParser {
+  /**
+   * Reads the configuration file and assign
+   * its contents to an object.
+   * @param   {string}              file the configuration file
+   * @returns {Record<string, any>}      an object representation of the configuration file
+   */
+  public parse(file: string): Record<string, any> {
+    return yaml.load(fs.readFileSync(file, 'utf-8'));
+  }
+}
