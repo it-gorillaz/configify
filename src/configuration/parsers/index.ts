f518ee728d7c456815ce950e92517ccde9c49bb9
tom-melo <neilton.demelo@emnify.com>
2024-01-25 17:05:26 +0000

    export json configuration parser

--- src/configuration/parsers/index.ts
+++ src/configuration/parsers/index.ts
@@ -1,3 +1,4 @@
 export * from './configuration-parser.factory';
 export * from './dotenv-configuration.parser';
+export * from './json-configuration.parser';
 export * from './yaml-configuration.parser';
