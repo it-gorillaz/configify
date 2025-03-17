88b947dcc06d1a246a7f164bec3b5edeb8491dfb
tom-melo <neilton.demelo@emnify.com>
2025-03-17 20:02:23 +0000

    export remote configuration strategy files

--- src/configuration/resolvers/index.ts
+++ src/configuration/resolvers/index.ts
@@ -1,2 +1,4 @@
 export * from './configuration-resolver.interface';
+export * from './remote-configuration-resolver.strategy';
+export * from './remote-configuration.resolver';
 export * from './resolved-value.interface';
