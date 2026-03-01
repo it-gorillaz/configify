96d16bce7d85b9f08264726bfd3ff1b88e843539
Tom Melo <mail2.tommelo@gmail.com>
2026-03-01 21:59:04 +0000

    add required args constructor decorator

---
+++ src/decorators/required-args-constructor.decorator.ts
@@ -0,0 +1,18 @@
+/**
+ * Marks a class as expecting constructor args payload for configuration.
+ * This is used by the module to determine if the class should be instantiated with constructor args.
+ */
+export const REQUIRED_ARGS_CONSTRUCTOR_METADATA = Symbol.for(
+  'REQUIRED_ARGS_CONSTRUCTOR_METADATA',
+);
+
+/**
+ * Class decorator to mark a class as expecting constructor args payload for configuration.
+ *
+ * @returns {ClassDecorator} the class decorator function to apply the metadata to the target class.
+ */
+export function RequiredArgsConstructor(): ClassDecorator {
+  return (target: Function) => {
+    Reflect.defineMetadata(REQUIRED_ARGS_CONSTRUCTOR_METADATA, true, target);
+  };
+}
